c60d75f0f6bf47bd986a1508afa2a5b4eb6d56e4
manvendra09 <ankursingh297@gmail.com>
2020-05-10 10:58:30 +0530

    nagivation changes

--- src/app/theme/layout/admin/navigation/navigation.ts
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -86,7 +86,7 @@ const NavigationItems = [
         title: 'Messeges',
         type: 'item',
         url: '/dashboard/analytics',
-        icon: 'feather icon-home'
+        icon: 'feather icon-message-square'
       },
       /*{
         id: 'page-layouts',
