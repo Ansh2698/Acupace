1ed4e45dd7cb185af7b64e270b6d78fc677e59eb
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-05-10 17:02:25 +0530

    Profile Section Added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -42,6 +42,11 @@ const routes: Routes = [
         path: 'sample-page',
         loadChildren: () => import('./demo/pages/sample-page/sample-page.module').then(module => module.SamplePageModule)
       }
+      ,
+      {
+        path:'profile',
+        loadChildren: () => import('./demo/pages/authentication/profile/profile.module').then(module => module.ProfileModule)
+      }
     ]
   },
   {
