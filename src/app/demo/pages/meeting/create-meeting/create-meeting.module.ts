c3b8d42cf8d8526d06cc8231dced3eed88808120
manvendra09 <ankursingh297@gmail.com>
2020-05-12 15:27:39 +0530

    all components in MyRoom added

--- src/app/demo/pages/meeting/create-meeting/create-meeting.module.ts
+++ src/app/demo/pages/meeting/create-meeting/create-meeting.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
 import { CreateMeetingRoutingModule } from './create-meeting-routing.module';
 import { CreateMeetingComponent } from './create-meeting.component';
 import {SharedModule} from '../../../../theme/shared/shared.module';
+import {NgbPopoverModule, NgbTooltipModule} from '@ng-bootstrap/ng-bootstrap';
+
 @NgModule({
   declarations: [CreateMeetingComponent],
   imports: [
     CommonModule,
     CreateMeetingRoutingModule,
-    SharedModule
+    SharedModule,
+    NgbTabsetModule,
+    NgbPopoverModule,
+    NgbTooltipModule
   ]
 })
 export class CreateMeetingModule { }
