fcd64c96cd1fb7edc90ad44a8719e43d9e7b14f9
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-05-14 14:13:03 +0530

    Agora Service Added for Video Streaming

--- src/app/demo/pages/sample-page/sample-page.component.ts
+++ src/app/demo/pages/sample-page/sample-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AgoraClient, ClientEvent, NgxAgoraService, Stream, StreamEvent } from 'ngx-agora';
 @Component({
   selector: 'app-sample-page',
   templateUrl: './sample-page.component.html',
@@ -7,9 +7,123 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SamplePageComponent implements OnInit {
 
-  constructor() { }
+  title = 'angular-video';
+  localCallId = 'agora_local';
+  remoteCalls: string[] = [];
 
-  ngOnInit() {
+  private client: AgoraClient;
+  private localStream: Stream;
+  private uid: number;
+
+  constructor(private ngxAgoraService: NgxAgoraService) {
+    this.uid = Math.floor(Math.random() * 100);
   }
 
+  ngOnInit() {
+    this.client = this.ngxAgoraService.createClient({ mode: 'rtc', codec: 'h264' });
+    this.assignClientHandlers();
+
+    this.localStream = this.ngxAgoraService.createStream({ streamID: this.uid, audio: true, video: true, screen: false });
+    this.assignLocalStreamHandlers();
+    // Join and publish methods added in this step
+    this.initLocalStream(() => this.join(uid => this.publish(), error => console.error(error)));
+  }
+
+  /**
+   * Attempts to connect to an online chat room where users can host and receive A/V streams.
+   */
+  join(onSuccess?: (uid: number | string) => void, onFailure?: (error: Error) => void): void {
+    this.client.join(null, 'foo-bar', this.uid, onSuccess, onFailure);
+  }
+
+  /**
+   * Attempts to upload the created local A/V stream to a joined chat room.
+   */
+  publish(): void {
+    this.client.publish(this.localStream, err => console.log('Publish local stream error: ' + err));
+  }
+
+  private assignClientHandlers(): void {
+    this.client.on(ClientEvent.LocalStreamPublished, evt => {
+      console.log('Publish local stream successfully');
+    });
+
+    this.client.on(ClientEvent.Error, error => {
+      console.log('Got error msg:', error.reason);
+      if (error.reason === 'DYNAMIC_KEY_TIMEOUT') {
+        this.client.renewChannelKey(
+          '',
+          () => console.log('Renewed the channel key successfully.'),
+          renewError => console.error('Renew channel key failed: ', renewError)
+        );
+      }
+    });
+
+    this.client.on(ClientEvent.RemoteStreamAdded, evt => {
+      const stream = evt.stream as Stream;
+      this.client.subscribe(stream, { audio: true, video: true }, err => {
+        if(err){
+          console.log('Subscribe stream failed', err);
+        }
+        else{
+          console.log("Succesfully added");
+        }
+      });
+    });
+    this.client.on(ClientEvent.RemoteStreamSubscribed, evt => {
+      const stream = evt.stream as Stream;
+      const id = this.getRemoteId(stream);
+      if (!this.remoteCalls.length) {
+        this.remoteCalls.push(id);
+        setTimeout(() => stream.play(id), 1000);
+      }
+    });
+
+    this.client.on(ClientEvent.RemoteStreamRemoved, evt => {
+      const stream = evt.stream as Stream;
+      if (stream) {
+        stream.stop();
+        this.remoteCalls = [];
+        console.log(`Remote stream is removed ${stream.getId()}`);
+      }
+    });
+
+    this.client.on(ClientEvent.PeerLeave, evt => {
+      const stream = evt.stream as Stream;
+      if (stream) {
+        stream.stop();
+        this.remoteCalls = this.remoteCalls.filter(call => call !== `${this.getRemoteId(stream)}`);
+        console.log(`${evt.uid} left from this channel`);
+      }
+    });
+  }
+
+  private assignLocalStreamHandlers(): void {
+    this.localStream.on(StreamEvent.MediaAccessAllowed, () => {
+      console.log('accessAllowed');
+    });
+
+    // The user has denied access to the camera and mic.
+    this.localStream.on(StreamEvent.MediaAccessDenied, () => {
+      console.log('accessDenied');
+    });
+  }
+
+  private initLocalStream(onSuccess?: () => any): void {
+    this.localStream.init(
+      () => {
+        // The user has granted access to the camera and mic.
+        this.localStream.play(this.localCallId);
+        if (onSuccess) {
+          onSuccess();
+        }
+      },
+      err => console.error('getUserMedia failed', err)
+    );
+  }
+
+  private getRemoteId(stream: Stream): string {
+    return `agora_remote-${stream.getId()}`;
+  }
 }
+
--- src/app/demo/pages/sample-page/sample-page.module.ts
+++ src/app/demo/pages/sample-page/sample-page.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { NgxAgoraModule } from 'ngx-agora';
 import { SamplePageRoutingModule } from './sample-page-routing.module';
 import { SamplePageComponent } from './sample-page.component';
 import {SharedModule} from '../../../theme/shared/shared.module';
@@ -10,7 +10,9 @@ import {SharedModule} from '../../../theme/shared/shared.module';
   imports: [
     CommonModule,
     SamplePageRoutingModule,
-    SharedModule
-  ]
+    SharedModule,
+    NgxAgoraModule.forRoot({AppID: 'c023596a5f6d4c949d9b207101ee8c74' })
+  ],
+  bootstrap: [SamplePageComponent]
 })
 export class SamplePageModule { }
