0e68210b30955c1c1b9e4b9446e48f3fa49c4c7f
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-05-25 18:25:49 +0530

    Host and Attended room id seperated

--- src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
+++ src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
@@ -37,7 +37,20 @@ export class NavRightComponent implements OnInit {
   Logout(){
     this.webservice.Logout_user();
   }
-  Join_Meeting(Notification:any){
+  Join_Meeting_by_Host(Notification:any){
+    this.current_time=moment();
+    if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time) && Notification.status!=2){
+      this.router.navigate(['/admin/sample-page'], { queryParams: { room_id:Notification.host_room_id,channel:Notification.channel_name,id:Notification.id} });
+    }
+    else{
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Either the Meeting Ended or You tried to enter before the Schedule',
+      })
+    }
+  }
+  Join_Meeting_by_attendee(Notification:any){
     this.current_time=moment();
     if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time) && Notification.status!=2){
       this.router.navigate(['/admin/sample-page'], { queryParams: { room_id:Notification.room_id,channel:Notification.channel_name,id:Notification.id} });
