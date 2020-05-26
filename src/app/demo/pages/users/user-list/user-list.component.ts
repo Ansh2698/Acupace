e88308fff9169b024aca07e9c0e2fb7653cba560
Ansh Agrawal <ansh.agrawal.che17@itbhu.ac.in>
2020-05-26 12:41:11 +0530

    Front Page Modified

--- src/app/demo/pages/users/user-list/user-list.component.ts
+++ src/app/demo/pages/users/user-list/user-list.component.ts
@@ -26,7 +26,7 @@ export class UserListComponent implements OnInit {
   }
   Join_Meeting(Notification:any){
     this.current_time=moment();
-    if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time)){
+    if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time) && Notification.meeting_status!=2){
       this.router.navigate(['/admin/sample-page'], { queryParams: { room_id:Notification.room_id,channel:Notification.channel_name,id:Notification.id} });
       // channel and notification_id added
     }
--- src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
+++ src/app/theme/layout/admin/nav-bar/nav-right/nav-right.component.ts
@@ -39,7 +39,7 @@ export class NavRightComponent implements OnInit {
   }
   Join_Meeting_by_Host(Notification:any){
     this.current_time=moment();
-    if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time) && Notification.status!=2){
+    if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time) && Notification.meeting_status!=2){
       this.router.navigate(['/admin/sample-page'], { queryParams: { room_id:Notification.host_room_id,channel:Notification.channel_name,id:Notification.id} });
     }
     else{
@@ -52,7 +52,7 @@ export class NavRightComponent implements OnInit {
   }
   Join_Meeting_by_attendee(Notification:any){
     this.current_time=moment();
-    if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time) && Notification.status!=2){
+    if(moment(Notification.host_meeting_end_time).isSameOrAfter(this.current_time) && moment(Notification.host_meeting_start_time).isSameOrBefore(this.current_time) && Notification.meeting_status!=2){
       this.router.navigate(['/admin/sample-page'], { queryParams: { room_id:Notification.room_id,channel:Notification.channel_name,id:Notification.id} });
     }
     else{
